69f3f4b5f5839b2891d5bb74e17455ae18f83f6d
Chris <chrismc16@outlook.com>
2023-12-30 15:59:11 -0800

    - Updated pubspec.yaml; Added multiple dependencies - Added icon_add.svg, icon_folder.svg - Updated icon_help.svg, icon_settings.svg; Changed stroke width - Updated [MANY]; Includes new dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,12 @@ import Foundation
 
 import screen_retriever
 import shared_preferences_foundation
+import system_theme
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SystemThemePlugin.register(with: registry.registrar(forPlugin: "SystemThemePlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
